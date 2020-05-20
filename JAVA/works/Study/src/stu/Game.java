f0d7656fa54c934f2fba0c74ecd18a3cef6b971b
bakaEC <1064071566@qq.com>
2020-05-20 15:46:04 +0800

    5.20更新

---
+++ JAVA/works/Study/src/stu/Game.java
@@ -0,0 +1,21 @@
+package stu;
+import java.util.*
+    /**
+     * 用户类
+     * 阶段1任务
+     * @author bakaEC
+     * @version 1.0
+     * */
+public static void main(String[]args){
+        String name = "zs";
+        int score = 0;
+        /**
+         * 出拳
+         * @return 出拳结果：1.石头 2.剪刀 3.布
+         */
+        }
+        public int showFist(){
+            //接收用户输入
+            Scanner input = new Scanner(System.in);
+            
+        }
