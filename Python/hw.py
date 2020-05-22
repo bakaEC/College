950dd8c9f3788d0005b203fcac8d1574808c8c55
bakaEC <1064071566@qq.com>
2020-05-22 12:27:01 +0800

    5.22 update

--- Python/hw.py
+++ Python/hw.py
@@ -1,1 +1,19 @@
-print("hello world")
+def my_abs (x):
+    if x>=0 : 
+        return x
+    else:
+        return -x
+def power(x,n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return x
+    elif n > 1:
+        s=1
+        while (n>0):
+            n=n-1
+            s=s*x
+    return s
+
+print(power(my_abs(-5),2))
+
