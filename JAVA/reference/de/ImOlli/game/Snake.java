028a4fd2df5904a9da78e911efee24a0bca25e79
bakaEC <1064071566@qq.com>
2020-06-19 17:59:58 +0800

    6.19 update

---
+++ JAVA/reference/de/ImOlli/game/Snake.java
@@ -0,0 +1,14 @@
+package de.ImOlli.game;
+
+public class Snake {
+
+    private static Game game;
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+}
