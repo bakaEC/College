7960ba87a29764826ea91820fa46ed740443e81e
bakaEC <1064071566@qq.com>
2020-08-17 03:23:09 +0800

    8.17 update

--- web/muma/homework/2.3.php
+++ web/muma/homework/2.3.php
@@ -1,21 +1,21 @@
-<meta charset=utf-8>
 <?php 
-    $name = $_POST["name"];
+    $name = $_POST["username"];
     $password = $_POST["password"];
-    $conn = mysqli_connect('127.0.0.1','root','root','users');      //  共用传参和链接数据库部分
+    $conn = mysqli_connect('127.0.0.1','root','root','test');   // 共用传参和链接数据库部分
    
     if(!empty($_POST['register'])) {        //点击注册按钮
-        $sql = "INSERT INTO user VALUE ('$name','$password')";
+        $sql = "INSERT INTO admin(username,password) VALUE ('$name','$password')";
         $result = mysqli_query($conn,$sql);
+var_dump($result);
         if($result){
             echo '注册成功';
         }else{
             echo '注册失败';
         }
     } elseif(!empty($_POST['login'])) {     //点击登陆按钮
-        $sql = "SELECT * FROM user WHERE name='$name'";
+        $sql = "SELECT * FROM admin WHERE username ='$name'";
         $result = mysqli_query($conn,$sql);
-        if(mysqli_fetch_array($result)['name'] == $name){
+        if(mysqli_fetch_array($result)['username'] == $name){
             echo "登录成功";
         }else{
             echo "登录失败!";
