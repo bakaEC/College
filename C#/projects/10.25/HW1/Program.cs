b859aec36d0649b7f8c12d5e0a5aef5cc6d2239e
bakaEC <1064071566@qq.com>
2020-11-02 00:34:41 +0800

    11.2 update

---
+++ C#/projects/10.25/HW1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Door door = new Door();
+            door.print();
+            door.printa();
+            door.hardness();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Act
+    {
+        public abstract void printa();
+        public abstract int hardness();
+    }
+
+    public interface Eyes
+    {
+        public abstract void print();
+    }
+
+    public class Door : Act,Eyes 
+    {
+        public void print()
+        {
+            Console.WriteLine("我是一个门");
+        }
+
+
+        public override int hardness()
+        {
+            Console.WriteLine("我有100耐久");
+            return 100;
+        }
+
+        public override void printa()
+        {
+            Console.WriteLine("我是sb");
+        }
+    }
+
+}
